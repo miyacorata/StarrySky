e10bac2d73ae1c9d695968521f191f8ed475e04a
K Miyano <miyano@miyacorata.net>
2020-06-18 22:45:25 +0900

    Add starrysky.config

---
+++ config/starrysky.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'version' => '0.1.0 alpha',
+    'repositoryUrl' => 'https://github.com/miyacorata/StarrySky',
+    'mastodonAccount' => 'https://mstdn.miyacorata.net/@StarrySky',
+];

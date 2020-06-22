b4bbe9ecd9ea8c85b268e2bc8afd8004baff6b77
K Miyano <miyano@miyacorata.net>
2020-06-22 10:51:17 +0900

    [WIP] Add star/index.blade

---
+++ resources/views/star/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app',['title' => '舞台少女一覧'])
+
+@section('head')
+@endsection
+
+@section('content')
+    <section id="list" class="plain-box">
+        <h1>舞台少女一覧</h1>
+        
+    </section>
+@endsection

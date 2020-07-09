bdd0f8ed815830a1d1a5e19e814698a334a84c0c
K Miyano <miyano@miyacorata.net>
2020-07-09 11:28:17 +0900

    Make Controller & Model & Migration

---
+++ database/migrations/2020_07_09_110309_create_pages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('slug')->unique();
+            $table->string('category')->nullable();
+            $table->boolean('cw'); // ネタバレ抑止
+            $table->string('color',7)->nullable();
+            $table->longText('document');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}

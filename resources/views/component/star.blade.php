ed9c48c017310839e4ab4c2e69c237f7331e9aed
K Miyano <miyano@miyacorata.net>
2020-09-06 03:27:59 +0900

    Update name processing (for zh,ko) (component/star.blade)

--- resources/views/component/star.blade.php
+++ resources/views/component/star.blade.php
@@ -2,10 +2,15 @@
 /**
  * @var $star App\Star
  */
+if($star->name_r_separate_secondary){
+    $name = separateString($star->name, $star->name_separate, '・');
+}else{
+    $name = $star->name;
+}
 ?>
 <a href="{{ url('/star/'.$star->name_r) }}" class="star-button" data-star="{{ $star->name_r }}">
     <div class="tachie" style="background-image: url('{{ asset('image/tachie/'.$star->name_r.'.png') }}')"></div>
-    <div class="name" style="background: {{ $star->color }}">{{ $star->name }}</div>
+    <div class="name" style="background: {{ $star->color }}">{{ $name }}</div>
     <div class="school-info">
         {{ $star->school }}<br>
         {{ $star->department.' '.$star->grade.($star->grade <= 3 ? '年' : '期生') }}

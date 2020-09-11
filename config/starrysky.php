5125ac659e0f1278d686a8aafba2192aad5c2a7d
K Miyano <miyano@miyacorata.net>
2020-09-12 06:40:16 +0900

    v1.0.1 Amaterasu

--- config/starrysky.php
+++ config/starrysky.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '1.0.0 Amaterasu',
+    'version' => '1.0.1 Amaterasu',
     'repositoryUrl' => 'https://github.com/miyacorata/StarrySky',
     'mastodonAccount' => 'https://mstdn.miyacorata.net/@StarrySky',
 ];

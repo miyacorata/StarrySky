e9ccb2aff918e61b51ff43b7282dcdac23e6e5ae
K Miyano <miyano@miyacorata.net>
2020-09-12 04:28:24 +0900

    Fix incorrect operator & add HTTP 503 message

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -11,15 +11,17 @@
         403 => 'Forbidden',
         404 => 'Not Found',
         500 => 'Internal Server Error',
+        503 => 'Service Unavailable',
     ];
     $messages = [
         400 => '要求の形式が正しくありません',
         403 => 'このページにアクセスする権限がありません',
         404 => 'お探しのページは存在しません',
         500 => 'サーバ内部でエラーが発生しました',
+        503 => 'アクセス集中かメンテナンスのためアクセスできません',
     ];
 
-    $message = $exception->getMessage() ?: $messages[$sc] ?: '何かがおかしいようです';
+    $message = $exception->getMessage() ?: $messages[$sc] ?? '何かがおかしいようです';
 ?>
 
 @section('head')
@@ -44,7 +46,7 @@
 @section('content')
     <div id="error">
         <img src="{{ asset('image/badge/403.png') }}" alt="403">
-        <h1><span>{{ $sc }}</span>{{ $status[$sc] ?: 'Something Wrong' }}</h1>
+        <h1><span>{{ $sc }}</span>{{ $status[$sc] ?? 'Something Wrong' }}</h1>
         <p>{{ $message }}</p>
     </div>
 @endsection
